7d6085968b9f04bf8bf65294d3d8b56bdfec8da4
Adnrey <lomovskoy.andrey@bk.ru>
2025-02-24 11:42:43 +0300

    add fn: create card and add fn: delete card

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/AndreyAlexsandrovich/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
+const template = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+const list = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(cardData) { 
+      const itemCopy = template.querySelector('.card').cloneNode(true);
+       itemCopy.querySelector(".card__title").textContent = cardData.name;
+       itemCopy.querySelector(".card__image").src = cardData.link;
+
+       return itemCopy;
+}
+
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) { 
+   cardElement.remove()
+}
 
 // @todo: Вывести карточки на страницу
+
+function showCards() { 
+   initialCards.forEach((cardData) => { 
+      const cardElement = createCard(cardData);
+      const deleteButton = cardElement.querySelector('.card__delete-button');
+      deleteButton.addEventListener('click', () => { 
+        deleteCard(cardElement);
+      })
+      list.append(cardElement);
+   })
+}
+
+showCards();
