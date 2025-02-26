cdf188e3e666dcd23f7ebdc58b07278efd025790
Adnrey <lomovskoy.andrey@bk.ru>
2025-02-26 10:17:54 +0300

    refactor: index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,20 @@
 // @todo: Темплейт карточки
-
 const template = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const list = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
-function createCard(cardData) { 
+function createCard(cardData, deleteCard) { 
       const itemCopy = template.querySelector('.card').cloneNode(true);
        itemCopy.querySelector(".card__title").textContent = cardData.name;
        itemCopy.querySelector(".card__image").src = cardData.link;
+       itemCopy.querySelector('.card__image').alt = cardData.name;
+
+       const deleteButton = itemCopy.querySelector('.card__delete-button');
+       deleteButton.addEventListener('click', () => { 
+          deleteCard(itemCopy);
+       });
 
        return itemCopy;
 }
@@ -21,14 +25,9 @@ function deleteCard(cardElement) {
 }
 
 // @todo: Вывести карточки на страницу
-
 function showCards() { 
    initialCards.forEach((cardData) => { 
-      const cardElement = createCard(cardData);
-      const deleteButton = cardElement.querySelector('.card__delete-button');
-      deleteButton.addEventListener('click', () => { 
-        deleteCard(cardElement);
-      })
+      const cardElement = createCard(cardData, deleteCard);
       list.append(cardElement);
    })
 }
